9b915872a65d12b20a4f8f8fe83cc0ebcbcda544
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-01-28 18:44:32 +0000

    Respond to prefers-reduced-motion

--- index.ts
+++ index.ts
@@ -6,12 +6,11 @@ const debug2 = document.getElementById('debug2')
 const client = myTheatre.getBoundingClientRect()
 const centrePointX = client.width / 2
 const centrePointY = client.height / 2
-debug1.innerText = `w: ${client.width} h: ${client.width} cX: ${centrePointX} cY: ${centrePointY}`
 
 myTheatre.addEventListener("pointermove", (e: PointerEvent) => {
   const x = (e.offsetX - centrePointX) / centrePointX
   const y = (e.offsetY - centrePointY) / centrePointY
-  debug2.innerText = `X: ${x} Y: ${y}`
+  debug2.innerText = `X: ${Math.round(x*100)}% Y: ${Math.round(y*100)}%`
   myTheatre.style.setProperty('--deltaX', `${x}`)
   myTheatre.style.setProperty('--deltaY', `${y}`)
 })
